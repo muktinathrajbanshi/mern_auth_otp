3df2ad3ef147ac7561a23388a27e8670636fc0d9
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-03 21:31:59 +0545

    la aja yati

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,8 +29,21 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
   } catch (error) {
     res.json({ success: false, message: error.message });
+  }
+};
+
+export const login = async () => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Email and password are required",
+    });
   }
 };
