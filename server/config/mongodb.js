6c2971fda7e9df8c28a51c38ca5dd4ab19b79a04
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-03 19:11:44 +0545

    oi dar lago my lord

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
 
-  await mongoose.connect(`{process.env.MONGODB_URI}/mern-auth`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
 };
 
 export default connectDb;
