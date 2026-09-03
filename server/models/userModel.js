77864f0cdaae22ff2dfce35675896b4381f7294d
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-03 19:34:08 +0545

    wah re bade vai kiya bath hain

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,4 +5,12 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   verifyOtp: { type: String, default: "" },
+  verifyOtpExpireAt: { type: Number, default: 0 },
+  isAccountVerified: { type: Boolean, default: false },
+  resetOtp: { type: String, default: "" },
+  resetOtpExpireAt: { type: Number, default: 0 },
 });
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
