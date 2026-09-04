3579a5fcd622613709cdcba6b449721166925e0d
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-04 15:33:16 +0545

    created the controller function for user login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -61,6 +61,19 @@ export const login = async () => {
     if (!isMatch) {
       return res.json({ success: false, message: "Invalid password" });
     }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({ success: true });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
