148bce05fec241ce3cbff9482010d67f53f3fb18
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-04 18:07:03 +0545

    wah re vai baklol kiya tarkif hain

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -80,3 +80,18 @@ export const login = async () => {
     return res.json({ success: false, message: error.message });
   }
 };
+
+//Logout part ok
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+
+    return res.json({ success: true, message: "Logged Out" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
