71703274948e7dc26d03f027d669d6f390e63767
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-06 20:51:41 +0545

    wow chalte raho rukna nahi chahiye

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -141,6 +141,8 @@ export const sendVerifyOtp = async (req, res) => {
   }
 };
 
+// Verify email account
+
 export const verifyEmail = async (req, res) => {
   const { userId, otp } = req.body;
 
@@ -154,6 +156,22 @@ export const verifyEmail = async (req, res) => {
     if (!user) {
       return res.json({ success: false, message: "User not found" });
     }
+
+    if (user.verifyOtp === "" || user.verifyOtp !== otp) {
+      return res.json({ success: false, message: "Invalid OTP" });
+    }
+
+    if (user.verifyOtpExpireAt < Date.now()) {
+      return res.json({ success: false, message: "OTP Expired" });
+    }
+
+    user.isAccountVerified = true;
+    user.verifyOtp = "";
+    user.verifyOtpExpireAt = 0;
+
+    await user.save();
+
+    return res.json({ success: true, message: "Email verified successfully" });
   } catch (error) {
     return res.json({ success: false, message: "Missing Details" });
   }
