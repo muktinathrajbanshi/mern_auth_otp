1c4ae03ee96525a05ea3494337b6d26055a38455
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-09 21:27:36 +0545

    wow kiya karname hain haha

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={assets.logo} alt="" className="w-28 sm:w-32" />
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Navbar;
