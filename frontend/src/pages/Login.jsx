e7474b63359478b61d81354dc5da94ab24cbb391
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-10 20:13:04 +0545

    oi it's looking good

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { assets } from "../assets/assets";
 
 const Login = () => {
-  const [state, setState] = useState("Sign up");
+  const [state, setState] = useState("Sign Up");
 
   return (
     <div>
@@ -11,6 +11,14 @@ const Login = () => {
         alt=""
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
       />
+      <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
+        <h2>{state === "Sign Up" ? "Create Account" : "Login"}</h2>
+        <p>
+          {state === "Sign Up"
+            ? "Create your account"
+            : "Login to your account!"}
+        </p>
+      </div>
     </div>
   );
 };
