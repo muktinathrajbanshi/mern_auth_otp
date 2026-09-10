b54bb26745bbe9a234a78f12b70b5e3fc271dc8f
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-10 20:30:37 +0545

    umm aba kai hunchha jasto chha hola

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,19 +5,31 @@ const Login = () => {
   const [state, setState] = useState("Sign Up");
 
   return (
-    <div>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
       <img
         src={assets.logo}
         alt=""
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
       />
-      <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
         <h2>{state === "Sign Up" ? "Create Account" : "Login"}</h2>
         <p>
           {state === "Sign Up"
             ? "Create your account"
             : "Login to your account!"}
         </p>
+
+        <form>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.person_icon} alt="img" />
+            <input
+              className="bg-transparent outline-none"
+              type="text"
+              placeholder="Full Name"
+              required
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
