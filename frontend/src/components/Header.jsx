9017526ec66f7d9dff1c03f3e723275fa3de3570
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-10 09:38:16 +0545

    wow it's looking good

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,10 @@ const Header = () => {
         alt=""
         className="w-36 h-36 rounded-full mb-6"
       />
-      <h1>Hey Developer</h1>
+      <h1>
+        Hey Developer{" "}
+        <img className="w-8 aspect-square" src={assets.hand_wave} alt="" />
+      </h1>
     </div>
   );
 };
