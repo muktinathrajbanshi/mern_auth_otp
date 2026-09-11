b8e37db5747211a087e401202dcdef7f7c7f5be9
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-11 09:26:38 +0545

    hai garmi

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,9 @@ import { assets } from "../assets/assets";
 
 const Login = () => {
   const [state, setState] = useState("Sign Up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
@@ -27,6 +30,8 @@ const Login = () => {
             <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
               <img src={assets.person_icon} alt="img" />
               <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 className="bg-transparent outline-none"
                 type="text"
                 placeholder="Full Name"
