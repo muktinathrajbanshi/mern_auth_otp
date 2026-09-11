cbfa682e5bf5190b9dfc8a16917466061813bae8
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-11 11:19:23 +0545

    la sutum sutam banai diyo vo aba tanab

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,12 +6,18 @@ import { AppContext } from "../context/AppContext";
 const Login = () => {
   const navigate = useNavigate();
 
-  const { backendUrl } = useContext(AppContext);
+  const { backendUrl, setIsLoggedIn } = useContext(AppContext);
 
   const [state, setState] = useState("Sign Up");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const onsubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+    } catch (error) {}
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
@@ -32,7 +38,7 @@ const Login = () => {
             : "Login to your account!"}
         </p>
 
-        <form>
+        <form onSubmit={onsubmitHandler}>
           {state === "Sign Up" && (
             <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
               <img src={assets.person_icon} alt="img" />
