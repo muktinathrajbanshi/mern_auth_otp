128fae7f633096ee579000f347e409f1f901ecd1
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-12 15:08:24 +0545

    aba rest garum la

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,6 +10,23 @@ const Navbar = () => {
   const { userData, backendUrl, setUserData, setIsLoggedIn } =
     useContext(AppContext);
 
+  const sendVerificationOtp = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+
+      const { data } = await axios.post(
+        backendUrl + "/api/auth/send-verify-otp",
+      );
+
+      if (data.success) {
+        navigate("/email-verify");
+        toast.success("data.message");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {}
+  };
+
   const logout = async () => {
     try {
       axios.defaults.withCredentials = true;
@@ -31,7 +48,7 @@ const Navbar = () => {
           {userData.name[0].toUpperCase()}
           <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
             <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
-              {userData.isAccountVerified && (
+              {!userData.isAccountVerified && (
                 <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">
                   Verify email
                 </li>
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -5,6 +5,8 @@ import { toast } from "react-toastify";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+  axios.defaults.withCredentials = true;
+
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState(false);
