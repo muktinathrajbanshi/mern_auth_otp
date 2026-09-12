7b730bbfc2819f0269a492d9c637f3b9ff5aaa6b
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-12 14:07:40 +0545

    ye kaise huwa

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,8 +16,13 @@ const Navbar = () => {
         <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
           {userData.name[0].toUpperCase()}
           <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
-            <ul>
-              <li>Verify email</li>
+            <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
+              <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">
+                Verify email
+              </li>
+              <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10">
+                Logout
+              </li>
             </ul>
           </div>
         </div>
