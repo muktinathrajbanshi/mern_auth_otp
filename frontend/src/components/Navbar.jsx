b623a9de14bda105506800dd67450090d67df720
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-14 18:06:18 +0545

    la kati dherai problme

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,12 +20,12 @@ const Navbar = () => {
 
       if (data.success) {
         navigate("/email-verify");
-        toast.success("data.message");
+        toast.success(data.message);
       } else {
         toast.error(data.message);
       }
     } catch (error) {
-      toast.error(error.message);
+      toast.error(error?.response?.data?.message || error.message);
     }
   };
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -111,7 +111,7 @@ export const logout = async (req, res) => {
 
 export const sendVerifyOtp = async (req, res) => {
   try {
-    const { userId } = req.body;
+    const userId = req.userId;
 
     const user = await userModel.findById(userId);
 
@@ -144,7 +144,8 @@ export const sendVerifyOtp = async (req, res) => {
 // Verify the Email using the OTP
 
 export const verifyEmail = async (req, res) => {
-  const { userId, otp } = req.body;
+  const { otp } = req.body;
+  const userId = req.userId;
 
   if (!userId || !otp) {
     return res.json({ success: false, message: "Missing Details" });
