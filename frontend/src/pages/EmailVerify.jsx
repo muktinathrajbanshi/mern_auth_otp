6af2031f681b960cd4dc9c02eec20c0a38e5b1b6
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-19 19:30:56 +0545

    so let's get started muktinath

--- frontend/src/pages/EmailVerify.jsx
+++ frontend/src/pages/EmailVerify.jsx
@@ -65,7 +65,7 @@ const EmailVerify = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
       <img
-        onClick={() => Navigate("/")}
+        onClick={() => navigate("/")}
         src={assets.logo}
         alt=""
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,15 +1,35 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const ResetPassword = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
       <img
-        onClick={() => Navigate("/")}
+        onClick={() => navigate("/")}
         src={assets.logo}
         alt=""
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
       />
+
+      {/* enter email id  */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Reset password
+        </h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter your registered email address
+        </p>
+        <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5c]">
+          <img src={assets.mail_icon} alt="" className="w-3 h-3" />
+          <input
+            type="email"
+            placeholder="Email id"
+            className="bg-transparent outline-none"
+          />
+        </div>
+      </form>
     </div>
   );
 };
