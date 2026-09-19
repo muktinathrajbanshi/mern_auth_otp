91891e492b5d941b629ce376ef1273cec1c38f16
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-19 19:51:35 +0545

    aja yo project sakaunu paro yar

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,8 +1,10 @@
 import { Navigate, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { useState } from "react";
 
 const ResetPassword = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
@@ -26,9 +28,18 @@ const ResetPassword = () => {
           <input
             type="email"
             placeholder="Email id"
-            className="bg-transparent outline-none"
+            className="bg-transparent outline-none text-white"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
+        <button
+          className="w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900
+        text-white rounded-full mt-3"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
