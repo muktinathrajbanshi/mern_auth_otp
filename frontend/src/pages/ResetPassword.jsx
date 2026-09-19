f70835f464e3d5cfb75ef4196018841f4b03b82e
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-19 20:52:35 +0545

    wow that's great job yaro idhar aaro

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,34 +1,35 @@
 import { Navigate, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const ResetPassword = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
-   const inputRefs = React.useRef([]);
-  
-    const handleInput = (e, index) => {
-      if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
-        inputRefs.current[index + 1].focus();
+  const inputRefs = React.useRef([]);
+
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData("text");
+    const pasteArray = paste.split("");
+    pasteArray.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
       }
-    };
-  
-    const handleKeyDown = (e, index) => {
-      if (e.key === "Backspace" && e.target.value === "" && index > 0) {
-        inputRefs.current[index - 1].focus();
-      }
-    };
-  
-    const handlePaste = (e) => {
-      const paste = e.clipboardData.getData("text");
-      const pasteArray = paste.split("");
-      pasteArray.forEach((char, index) => {
-        if (inputRefs.current[index]) {
-          inputRefs.current[index].value = char;
-        }
-      });
-    };
+    });
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
@@ -67,10 +68,7 @@ const ResetPassword = () => {
       </form>
 
       {/* otp input form  */}
-      <form
-        onSubmit={}
-        className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm"
-      >
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
         <h1 className="text-white text-2xl font-semibold text-center mb-4">
           Reset password OTP
         </h1>
@@ -94,7 +92,34 @@ const ResetPassword = () => {
             ))}
         </div>
         <button className="w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
-          Verify email
+          Submit
+        </button>
+      </form>
+
+      {/* enter new password  */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          New password
+        </h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter the new password below
+        </p>
+        <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5c]">
+          <img src={assets.lock_icon} alt="" className="w-3 h-3" />
+          <input
+            type="password"
+            placeholder="Password"
+            className="bg-transparent outline-none text-white"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button
+          className="w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900
+        text-white rounded-full mt-3"
+        >
+          Submit
         </button>
       </form>
     </div>
